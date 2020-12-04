173d35ddca4f278c9dc7bfa075946962c35a3c8b
github username <github email address>
2020-12-04 13:40:32 -0500

    pic fix

--- src/components/About.js
+++ src/components/About.js
@@ -29,7 +29,7 @@ handlesHideImg= () =>{
       <div className="textBio">
         <h1>About Me</h1>
 
-        <h3>I am a full stack web developer with a passion for creating optimized, user friendly web applications with superior functionality and accessibility.  I'm a driven leader with an endless desire to learn, grow, and overcome obstacles.  I have experience working with JavaScript, React.js, Ruby, Ruby on Rails, ActiveRecord, Sinatra, SQL, HTML and CSS.  I bring strong communication and leadership skills, ensuring excellent group cohesiveness, maximizing productivity and workflow.</h3>
+        <h3>I am a Software developer/Data engineering consultant with a passion for creating optimized, user friendly applications with superior functionality and accessibility. I’m a self-motivated, continuous learner with a flexible skillset and the ability to pick-up new tasks with ease. I have vast experience working with languages and frameworks such as JavaScript, React.js, Ruby, Ruby on Rails, and other fullstack tools. In addition, I have worked with Big Data technologies like Spark, SQL, and Informatica. I’m a driven leader with an endless desire to learn, grow, and overcome obstacles.   I bring strong communication skills, ensuring excellent group cohesiveness, maximizing productivity and workflow.</h3>
       </div>
 
       <div className="paulBox" onPointerEnter={this.handlesShowImg} onPointerLeave={this.handlesHideImg} width="auto" height="315">
@@ -39,12 +39,12 @@ handlesHideImg= () =>{
             <img
               target="_blank"
               href="https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/69942897_2397639260284490_4172973680579575808_n.jpg?_nc_cat=108&_nc_oc=AQkzKqEKNXAD6hAlR5N8XuEyc3miTi38ABIKlMkoavOA_srNn9I4FHLYoms0o70lois&_nc_ht=scontent-lga3-1.xx&oh=0f0c43c2cbf0e085c88d8d8003f49346&oe=5DFFA15E" className='mePic'
-              src='https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/69942897_2397639260284490_4172973680579575808_n.jpg?_nc_cat=108&_nc_oc=AQkzKqEKNXAD6hAlR5N8XuEyc3miTi38ABIKlMkoavOA_srNn9I4FHLYoms0o70lois&_nc_ht=scontent-lga3-1.xx&oh=0f0c43c2cbf0e085c88d8d8003f49346&oe=5DFFA15E' alt='Picture of Paul Riverain'
+              src='https://scontent-bos3-1.xx.fbcdn.net/v/t1.0-9/69942897_2397639260284490_4172973680579575808_n.jpg?_nc_cat=108&ccb=2&_nc_sid=730e14&_nc_ohc=mshOG4azcGcAX8TJP3c&_nc_ht=scontent-bos3-1.xx&oh=6d8e55a7a745ef035b70cf299a1dabe0&oe=5FF019A2' alt='Picture of Paul Riverain'
             />
 
 
           :
-            <img target="_blank" href="https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/69718252_2397639420284474_3902879289166528512_n.jpg?_nc_cat=101&_nc_oc=AQmcbw3uawZ76u4iPAjIxtmyHexF08ECOI8STj0PiBbjFoTQirjYyta8mkRzNy-alQI&_nc_ht=scontent-lga3-1.xx&oh=0d92b59aa972a2f9cd907fa8610cca0c&oe=5E134D71" className='mePic' src='https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/69718252_2397639420284474_3902879289166528512_n.jpg?_nc_cat=101&_nc_oc=AQmcbw3uawZ76u4iPAjIxtmyHexF08ECOI8STj0PiBbjFoTQirjYyta8mkRzNy-alQI&_nc_ht=scontent-lga3-1.xx&oh=0d92b59aa972a2f9cd907fa8610cca0c&oe=5E134D71' alt='Picture of Paul Riverain in black and white'/>
+            <img target="_blank" href="https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/69718252_2397639420284474_3902879289166528512_n.jpg?_nc_cat=101&_nc_oc=AQmcbw3uawZ76u4iPAjIxtmyHexF08ECOI8STj0PiBbjFoTQirjYyta8mkRzNy-alQI&_nc_ht=scontent-lga3-1.xx&oh=0d92b59aa972a2f9cd907fa8610cca0c&oe=5E134D71" className='mePic' src='https://scontent-bos3-1.xx.fbcdn.net/v/t1.0-9/69718252_2397639420284474_3902879289166528512_n.jpg?_nc_cat=101&ccb=2&_nc_sid=730e14&_nc_ohc=hVD37SfjQooAX_XsbYS&_nc_ht=scontent-bos3-1.xx&oh=0ea9fc1607906f43916ca29514fd11cd&oe=5FF0340F' alt='Picture of Paul Riverain in black and white'/>
           }
         </div>
 
--- src/containers/ProjectPage.js
+++ src/containers/ProjectPage.js
@@ -18,19 +18,19 @@ class ProjectPage extends Component {
 
               <div className='projectCard'>
                 <h2>Language Ladder</h2>
-                <a target="_blank" href="https://www.youtube.com/watch?v=_a73TgHXshc&feature=youtu.be"><img src='https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/67375803_2320895117958905_1299386473585311744_n.jpg?_nc_cat=107&_nc_oc=AQnk6MamOE-VCoDkgre1wxdlPWGJuZ7PvsYP-hyDijGTROxXNKn1kHYENfrUxeU4P24&_nc_ht=scontent-lga3-1.xx&oh=823582a82a72ec74e53ed0851c4da935&oe=5DEDDF34'/></a>
+                <a target="_blank" href="https://www.youtube.com/watch?v=_a73TgHXshc&feature=youtu.be"><img src='https://scontent-bos3-1.xx.fbcdn.net/v/t1.0-9/67193583_2320895114625572_4146585796649943040_o.jpg?_nc_cat=106&ccb=2&_nc_sid=730e14&_nc_ohc=sRWD4U6KgmYAX_0j7EM&_nc_ht=scontent-bos3-1.xx&oh=b0e31a12e7ac713a35420e93341725bb&oe=5FEEE4C0'/></a>
                 <h5><a target="_blank" href="https://www.youtube.com/watch?v=_a73TgHXshc&feature=youtu.be">Language Ladder</a> is an interactive single-page Translator site that enables users to climb over any language barriers they encounter.</h5>
               </div>
 
               <div className='projectCard'>
                 <h2>Rate My Instructor  (<a target="_blank" href="https://rate-my-instructor-frontend.herokuapp.com/">Demo</a>)</h2>
-                <a target="_blank" href="https://www.youtube.com/watch?v=8ZFzV62FAq8&feature=youtu.be"><img src="https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/67145031_2320895317958885_2752462334608801792_n.jpg?_nc_cat=109&_nc_oc=AQkeai6y5j4CkXhM36vaNCqE_zls9HCFSr5m2Z_yUSSaKHyTKmg07RA8p_koJUuz43s&_nc_ht=scontent-lga3-1.xx&oh=3cfc8b6c0dcb86c12d2700c8e7b14da3&oe=5DB80DA8"/></a>
+                <a target="_blank" href="https://www.youtube.com/watch?v=8ZFzV62FAq8&feature=youtu.be"><img src="https://scontent-bos3-1.xx.fbcdn.net/v/t1.0-9/67580026_2320895321292218_5294043948237455360_o.jpg?_nc_cat=110&ccb=2&_nc_sid=730e14&_nc_ohc=pVqISbD8CTkAX_2ud7Z&_nc_ht=scontent-bos3-1.xx&oh=1e6f9b860a35a2ebec9dc1251f013573&oe=5FF08F08"/></a>
                 <h5><a target="_blank" href="https://www.youtube.com/watch?v=8ZFzV62FAq8&feature=youtu.be">Rate My Instructor</a> is a feature packed application that gives students the power to rate and comment on their software bootcamp instructors.</h5>
               </div>
 
               <div className='projectCard'>
                 <h2>Riverain Recreation</h2>
-                <a target="_blank" href="https://www.youtube.com/watch?v=mfflU4s5Xlk&feature=youtu.be"><img src="https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/67446080_2320895524625531_2234157153185169408_n.jpg?_nc_cat=105&_nc_oc=AQnFs3q-eSt4cyb7H9IG7ggnZrYvW7wBrMwgjA0bomN3lyWRRiftkDPMH6LYy-qnUZM&_nc_ht=scontent-lga3-1.xx&oh=ed00bb31296238bbe09c0549c1046118&oe=5DE9C708"/></a>
+                <a target="_blank" href="https://www.youtube.com/watch?v=mfflU4s5Xlk&feature=youtu.be"><img src="https://scontent-bos3-1.xx.fbcdn.net/v/t1.0-9/67406968_2320895864625497_2303177105833197568_o.jpg?_nc_cat=108&ccb=2&_nc_sid=730e14&_nc_ohc=mGjvkoBW5z4AX8t2L5s&_nc_ht=scontent-bos3-1.xx&oh=609fef68d3b63686a7afb9dedc9e0efa&oe=5FEE2135"/></a>
                 <h5><a target="_blank" href="https://www.youtube.com/watch?v=mfflU4s5Xlk&feature=youtu.be">Riverain Recreation</a> is a Ruby on Rails monolithic application for adventurous individuals looking to reserve spots on posted recreational trips.</h5>
               </div>
             </div>
